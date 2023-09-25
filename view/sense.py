ca59cb17310e50367e25aa90bb1cc7d5987726cc
pedro <pedrofrs008@gmail.com>
2023-09-25 17:11:34 -0300

    Update: BatAlarme

--- view/sense.py
+++ view/sense.py
@@ -7,15 +7,23 @@ from tkinter import Listbox, Entry, Button, Scrollbar
 import threading
 import datetime
 import schedule
+from playsound import playsound
 
 recognizer = sr.Recognizer()
 listaHorarios = []
 
-def disparar_alarme(lembrete):
+def disparar_alarme(lembrete, horarioAux):
+    playsound(r'C:\Users\pedro\OneDrive\Área de Trabalho\TrabalhoIA\view\batAlarme.mp3')
+    time.sleep(0.25)
     text_to_speech(lembrete)
+    print(horarioAux)
+    if horarioAux in listaHorarios:
+       listaHorarios.remove(horarioAux)
+       atualizar_lista()
 
-def definir_alarme(hora_alarme, lembrete):
+def definir_alarme(hora_alarme, lembrete, horarioAux):
   try:
+      horario = hora_alarme
       # Divida a hora e os minutos da string fornecida
       hora, minutos = map(int, hora_alarme.split(':'))
       
@@ -34,8 +42,7 @@ def definir_alarme(hora_alarme, lembrete):
           hora_alarme += datetime.timedelta(days=1)
       
       # Agende o alarme com o schedule
-      schedule.every().day.at(hora_alarme.strftime("%H:%M")).do(disparar_alarme, lembrete=lembrete)
-  
+      schedule.every().day.at(hora_alarme.strftime("%H:%M")).do(disparar_alarme, lembrete=lembrete, horarioAux=horarioAux)
   except ValueError:
       print("Formato de hora inválido. Use o formato '00:00'.")
 
@@ -143,6 +150,7 @@ def speech_to_text(flag):
 
 def horario_alarme():
   
+  horarioAux = ""
   while True:
     sent = 0
     while sent != 1:
@@ -158,20 +166,25 @@ def horario_alarme():
     sent = 0
     text_to_speech("Qual o horário do alarme?")
     print ("Fale o horário:")
+
     while sent != 1:
       arrTextHoras = speech_to_text(0)
       arrTextHoras = arrTextHoras.split(' ')
       horario = find_horario(arrTextHoras)
       if horario:
         print("Horário identificado:", horario)
-        listaHorarios.append(horario)
-        atualizar_lista()
         sent = 1
+
     text_to_speech("Grave seu lembrete")
     print ("Fale o lembrete:")
     lembrete = speech_to_text(0)
+    
     print("Lembrete gravado:", lembrete)
-    definir_alarme(horario, lembrete)
+    horarioAux = horario + " - " + lembrete
+
+    definir_alarme(horario, lembrete,horarioAux)
+    listaHorarios.append(horarioAux)
+    atualizar_lista()
     
 
   
